8f40e42b39a22c8755d4840163131d8300af69a5
[Muniru Issah] <[imuniru@st.ug.edu.gh]>
2023-04-19 08:46:40 +0000

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `frontendmento-main`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
