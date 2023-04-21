21ae0aba1f3eef4e6900880ceee9a5d287f0bd4f
[Muniru Issah] <[imuniru@st.ug.edu.gh]>
2023-04-21 20:40:16 +0000

    misc

--- src/features/order-summary-component/components/OrderCard.js
+++ src/features/order-summary-component/components/OrderCard.js
@@ -5,7 +5,7 @@ import hero from "../images/illustration-hero.svg";
 
 const OrderCard = () => {
     return (
-        <div className={'bg-white max-w-[400px] rounded-2xl shadow-lg shadow-[#3829E0]/10 overflow-hidden'}>
+        <div className={'bg-white max-w-[450px] rounded-2xl shadow-lg shadow-[#3829E0]/10 overflow-hidden'}>
             <img src={hero} className={'object-fit'} alt={'hero'} />
             <div className={' py-7 md:py-10 px-7 text-center flex flex-col items-center space-y-5'}>
                 <h2 className={'text-2xl font-bold text-[#1F2F56]'}>Order Summary</h2>
