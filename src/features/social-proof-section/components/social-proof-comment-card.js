74379c5f15f33a4f1adf88be894313959e72613f
[Muniru Issah] <[imuniru@st.ug.edu.gh]>
2023-05-19 14:42:22 +0000

    misc

--- src/features/social-proof-section/components/social-proof-comment-card.js
+++ src/features/social-proof-section/components/social-proof-comment-card.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const SocialProofCommentCard = ({image,name,title,description,extraClasses}) => {
     return (
-        <div className={`w-92 h-10/12 p-8 bg-[#502050] rounded-lg flex flex-col gap-6 ${extraClasses} hover:-translate-y-5 hover:self-start transition-all ease-in-out`}>
+        <div className={`w-92 h-10/12 p-8 bg-[#502050] rounded-lg flex flex-col gap-6 ${extraClasses}`}>
             <div className={'flex items-center gap-5'}>
                 <img src={image} alt="" className="w-10 rounded-full"/>
                 <div className={'flex flex-col'}>
